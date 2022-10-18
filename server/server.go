4e9c9c6600c4af538bae14ecb62825315e85455b
Luis David Barrios <cyberluisda@gmail.com>
2022-10-18 12:44:05 +0200

    [server] Initial implementation of PayloadStorage
    Struct to manage payloads received by the listeners

--- server/server.go
+++ server/server.go
@@ -5,6 +5,11 @@ The payload items can be listed while server is running of after it is closed
 */
 package server
 
+import (
+	"regexp"
+	"sync"
+)
+
 type BasicServer interface {
 	// Start starts the server (listener) and enable the input data processing.
 	Start() error
@@ -38,4 +43,74 @@ type BasicServer interface {
 
 	//Connections return the number of active connections.
 	Connections() int
+}
+
+type PayloadStorage struct {
+	payloads    map[string][]byte
+	payloadsMtx sync.RWMutex
+}
+
+func (ps *PayloadStorage) InitPayload() {
+	if ps.payloads == nil {
+		ps.payloads = make(map[string][]byte)
+	}
+}
+
+// NPayloadItems returns the number of payloads items received by clients.
+func (ps *PayloadStorage) NPayloadItems() int {
+	ps.payloadsMtx.RLock()
+	defer ps.payloadsMtx.RUnlock()
+	return len(ps.payloads)
+}
+
+//Reset cleans the list of payloads received until now.
+func (ps *PayloadStorage) Reset() {
+	ps.payloadsMtx.Lock()
+	defer ps.payloadsMtx.Unlock()
+	for k := range ps.payloads {
+		delete(ps.payloads, k)
+	}
+}
+
+func (ps *PayloadStorage) AddPayload(addr string, buffer []byte, n int) {
+	ps.payloadsMtx.Lock()
+	defer ps.payloadsMtx.Unlock()
+
+	v, ok := ps.payloads[addr]
+	if ok {
+		v = append(v, buffer[0:n]...)
+	} else {
+		v = make([]byte, n)
+		copy(v, buffer)
+	}
+	ps.payloads[addr] = v
+}
+
+// GetPayloadAddresses returns the list of source address of the clients sent data.
+func (ps *PayloadStorage) GetPayloadAddresses() []string {
+	ps.payloadsMtx.RLock()
+	defer ps.payloadsMtx.RUnlock()
+	r := make([]string, len(ps.payloads))
+	i := 0
+	for k := range ps.payloads {
+		r[i] = k
+		i = i + 1
+	}
+
+	return r
+}
+
+//GetPayload returns the list of payloads received by a client identified with its source address.
+func (ps *PayloadStorage) GetPayload(remoteAddr string) []byte {
+	ps.payloadsMtx.RLock()
+	defer ps.payloadsMtx.RUnlock()
+	return ps.payloads[remoteAddr]
+}
+
+//GetPayloads returns the full list of payloads in a map which key is the source address of the client.
+func (ps *PayloadStorage) GetPayloads() map[string][]byte {
+	ps.payloadsMtx.RLock()
+	defer ps.payloadsMtx.RUnlock()
+
+	return ps.payloads
 }
