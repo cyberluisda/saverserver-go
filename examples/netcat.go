75892632701319c44b288dacb496bc0d98ca7f46
Luis David Barrios <cyberluisda@gmail.com>
2022-10-24 18:07:17 +0200

    TLS supported by netcat example app

--- examples/netcat.go
+++ examples/netcat.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -11,14 +14,38 @@ import (
 	"github.com/cyberluisda/saverserver-go/server"
 )
 
-var user bool
-
 func main() {
-	if len(os.Args) < 2 {
+	var tlsKey string
+	flag.StringVar(&tlsKey, "tlskey", "", "Key file to enable tls (over connections like tcp)")
+	var tlsCert string
+	flag.StringVar(&tlsCert, "tlscert", "", "Certificate file to enable tls (over connections like tcp)")
+	var user bool
+	flag.BoolVar(&user, "user", false, "If true, client authentication with certificate is required")
+	var display bool
+	flag.BoolVar(
+		&display,
+		"display",
+		false,
+		"If true, address and user certificate info (if available) is displayed before each incoming data",
+	)
+	var address string
+	flag.StringVar(
+		&address,
+		"address",
+		"",
+		"listening address, protocol included, Examples: udp://localhost:12000, tcp://localhost:13000. "+
+			"To choose a random port: tcp://localhost:0 or udp://localhost:0")
+	flag.Parse()
+
+	if address == "" {
 		fmt.Printf(
-			`Usage: %s address.
+			`Usage: %s [ --tlskey keyfile.key --tlscert certfile.crt [ --user ] ] [ --display ] --address address.
 	address is the listening address, protocol included, Examples: udp://localhost:12000, tcp://localhost:13000.
-	With random ports: tcp://localhost:0 or udp://localhost:0
+	To choose random ports: tcp://localhost:0 or udp://localhost:0
+
+	keyfile.key and certfile.crt are the key and certificate file to enable tls connections. Only valid for tcp
+	and similar connections.
+
 `,
 			os.Args[0],
 		)
@@ -30,7 +57,7 @@ func main() {
 	listenerProtocols := []string{"tcp", "tcp4", "tcp6", "unix", "unixpacket"}
 	isListener := false
 	for _, proto := range listenerProtocols {
-		if strings.HasPrefix(os.Args[1], proto) {
+		if strings.HasPrefix(address, proto) {
 			isListener = true
 			break
 		}
@@ -44,7 +71,35 @@ func main() {
 		return false
 	}
 
-	if isListener {
+	if tlsKey != "" && tlsCert != "" {
+		bsKey, err := ioutil.ReadFile(tlsKey)
+		if err != nil {
+			log.Fatalf("Error while load key file %s: %v", tlsKey, err)
+		}
+
+		bsCert, err := ioutil.ReadFile(tlsCert)
+		if err != nil {
+			log.Fatalf("Error while load certificate file %s: %v", tlsCert, err)
+		}
+
+		tll := server.TLSListener{
+			ConnectionMgr: server.ConnectionMgr{
+				Address: address,
+			},
+			PayloadStorage: server.PayloadStorage{
+				CallBack: callFunc,
+			},
+			KeyPem:  bsKey,
+			CertPem: bsCert,
+		}
+
+		if user {
+			tll.ClientAuth = tls.RequireAnyClientCert
+		}
+
+		bs = &tll
+
+	} else if isListener {
 		lst := server.Listener{
 			ConnectionMgr: server.ConnectionMgr{
 				Address: address,
